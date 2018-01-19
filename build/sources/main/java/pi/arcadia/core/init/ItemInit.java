75e30b8ea9850ace6e7af710c959cd5f2ca94bb2
LenaAlcie <ht222fd@student.lnu.se>
2018-01-19 21:52:21 +0100

    Items materials
    - plastotextile recipe
    - nuclear matter: itembase + recipe
    - nuclear core: itembase + recipe

--- build/sources/main/java/pi/arcadia/core/init/ItemInit.java
+++ build/sources/main/java/pi/arcadia/core/init/ItemInit.java
@@ -12,4 +12,6 @@ public class ItemInit {
 	public static final List <Item> ITEMS = new ArrayList<Item>();
 	
 	public static final Item PLASTOTEXTILE = new ItemBase("plastotextile");
+	public static final Item NUCLEAR_MATTER = new ItemBase("nuclear_matter");
+	public static final Item NUCLEAR_CORE = new ItemBase("nuclear_core");
 }
